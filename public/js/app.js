545efadde7f2c117d8582f41c80029fa44130026
Alexsandr <alexnoobgordon@gmail.com>
2022-04-28 16:57:23 +0300

    Создание четвертого таба

--- public/js/app.js
+++ public/js/app.js
@@ -47,7 +47,7 @@
 
 };*/
 
-var main = function() {
+/*var main = function() {
     "use strict";
     var tabNumber;
     for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
@@ -58,4 +58,17 @@ var main = function() {
             return false;
         });
     }
+};*/
+
+var main = function() {
+    "use strict";
+    $(".tabs a span").toArray().forEach(function(element) {
+        //создаем обработчик щелчков для этого элемента
+        $(element).on("click", function() {
+            $(".tabs a span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+            return false;
+        });
+    });
 };
