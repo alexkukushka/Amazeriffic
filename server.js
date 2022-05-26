d59579760d64234095a15935b63ee4e37bc3ba51
Alexsandr <alexnoobgordon@gmail.com>
2022-05-26 17:04:38 +0300

    Завершение практики №7

--- server.js
+++ server.js
@@ -1,48 +1,49 @@
 var express = require("express"),
     http = require("http"),
-    app = express(),
-    toDos = [{
-            "description": "Купить продукты",
-            "tags": ["шопинг", "рутина"]
-        },
-        {
-            "description": "Сделать несколько новых задач",
-            "tags": ["писательство", "работа"]
-        },
-        {
-            "description": "Подготовиться к лекции в понедельник",
-            "tags": ["работа", "преподавание"]
-        },
-        {
-            "description": "Ответить на электронные письма",
-            "tags": ["работа"]
-        },
-        {
-            "description": "Вывести Грейси на прогулку в парк",
-            "tags": ["рутина", "питомцы"]
-        },
-        {
-            "description": "Закончить писать книгу",
-            "tags": ["писательство", "работа"]
-        }
-    ]
-http.createServer(app).listen(3000);
+    mongoose = require("mongoose"),
+    app = express();
+
+mongoose.connect('mongodb://localhost/amazeriffic');
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [String]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
 app.post("/todos", function(req, res) {
-    var newToDo = req.body;
-    console.log(newToDo);
-    toDos.push(newToDo);
-    res.json({ "message": "Вы размещаетесь на сервере!" })
-})
+    console.log(req.body);
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    });
+    newToDo.save(function(err, result) {
+        if (err !== null) {
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            ToDo.find({}, function(err, result) {
+                if (err !== null) {
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
+    });
+});
 
 
 app.get("/todos.json", function(req, res) {
-    res.json(toDos);
+    ToDo.find({}, function(err, toDos) {
+        res.json(toDos);
+    });
 });
 
 
 app.get("/tabs", function(req, res) {
     res.sendFile(__dirname + "/public/tabs.html")
 })
+
+http.createServer(app).listen(3000);
